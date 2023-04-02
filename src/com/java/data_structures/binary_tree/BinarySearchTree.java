d5c84b69b7df153fbd23ae068ef333611c6eabb2
guilherme <guilhermelaviola96@gmail.com>
2023-04-02 20:03:37 -0300

    Included the test class to the Binary Tree example.

---
+++ src/com/java/data_structures/binary_tree/BinarySearchTree.java
@@ -0,0 +1,23 @@
+package com.java.data_structures.binary_tree;
+
+public class BinarySearchTree {
+	// Testing the BinaryTree class
+	public static void main(String[] args) {
+		BinaryTree bst = new BinaryTree();
+		bst.insert(2);
+		bst.insert(3);
+		bst.insert(1);
+		bst.insert(3);
+		bst.insert(2);
+		//bst.printTree();
+		//bst.printPostorder();
+		//bst.printPreorder();
+		System.out.println(bst.lookup(5));
+		System.out.println(bst.lookup(2));
+		//System.out.println(bst.size());
+		bst.insert(0);
+		bst.insert(-3);
+		//bst.printTree();
+		//System.out.println(bst.maxDepth());
+	}
+}
