8d82987e7308db9e0b8c595ebb098ef7b5e6a398
guilherme <guilhermelaviola96@gmail.com>
2023-04-13 21:58:33 -0300

    Tested BinaryTree methods and fixed some errors.

--- src/com/java/data_structures/binary_tree/BinarySearchTree.java
+++ src/com/java/data_structures/binary_tree/BinarySearchTree.java
@@ -1,9 +1,13 @@
 package com.java.data_structures.binary_tree;
 
+import com.java.data_structures.binary_tree.BinaryTree.Node;
+
 public class BinarySearchTree {
 	// Testing all the methods from BinaryTree class
 	public static void main(String[] args) {
+		Node nd = new Node(0);
 		BinaryTree bst = new BinaryTree();
+		
 		bst.insert(2);
 		bst.insert(3);
 		bst.insert(1);
@@ -13,23 +17,23 @@ public class BinarySearchTree {
 		bst.inOrder();
 		bst.postOrder();
 		bst.findMin();
-		//bst.findMax(null);
+		bst.findMax(nd);
 		bst.size();
 		bst.maxDepth();
 		bst.contains(0);
 		bst.contains(100);
-		bst.printBreadthCourse();
+		//bst.printBreadthCourse();
 		bst.breadthCourse();
-		bst.FB(null);
+		bst.FB(nd);
 		bst.displayNodesAndFB();
 		bst.breadthFirstSearch();
 		bst.lookup(2);
-		//System.out.println(bst.lookup(5));
-		//System.out.println(bst.lookup(2));
-		//System.out.println(bst.size());
+		System.out.println(bst.lookup(5));
+		System.out.println(bst.lookup(2));
+		System.out.println(bst.size());
 		bst.insert(0);
 		bst.insert(-3);
 		//bst.printTree();
-		//System.out.println(bst.maxDepth());
+		System.out.println(bst.maxDepth());
 	}
 }
