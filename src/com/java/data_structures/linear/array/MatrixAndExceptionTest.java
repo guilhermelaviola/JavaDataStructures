be21c0e790807a04b043fbdc2fce99c3508e560d
guilherme <guilhermelaviola96@gmail.com>
2023-06-07 12:17:12 -0300

    Updated Arrays.

--- src/com/java/data_structures/linear/array/MatrixAndExceptionTest.java
+++ src/com/java/data_structures/linear/array/MatrixAndExceptionTest.java
@@ -1,36 +1,44 @@
 package com.java.data_structures.linear.array;
 
 public class MatrixAndExceptionTest {
+	
 	public static void main(String[] args) {
 		Array a = new Array(2, 3);
 		Array b = new Array(2, 3);
 		Array c = new Array(3, 2);
 		
 		try {
-			
-		} catch {
-			
+			double[][] aux = {{1, 2, 3}, {4, 5, 6}};
+			a.setArray(aux);
+		} catch(MyException me) {
+			System.out.println(me);
 		}
 		try {
-			
-		} catch {
-			
+			double[][] aux = {{4, 5, 6}, {7, 8, 9}};
+			b.setArray(aux);
+		} catch(MyException me) {
+			System.out.println(me);
 		}
 		try {
-			
-		} catch {
-			
+			double[][] aux = {{1, 4}, {2, 5}, {3, 6}};
+			c.setArray(aux);
+		} catch(MyException me) {
+			System.out.println(me);
 		}
-		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
 		try {
-			
-		} catch {
-			
+			Array d = a.sumWith(b);
+			System.out.println(d);
+		} catch(MyException me) {
+			System.out.println(me);
 		}
 		try {
-			
-		} catch {
-			
+			Array d = a.sumWith(c);
+			System.out.println(d);
+		} catch(MyException me) {
+			System.out.println(me);
 		}
 	}
 }
